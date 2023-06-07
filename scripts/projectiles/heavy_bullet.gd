7eba2b48c2c565900ebfb9a880ebf94ffa5f929c
BoiLudens <mattacoker@gmail.com>
2023-06-06 21:22:59 -0400

    added crop-duster

---
+++ scripts/projectiles/heavy_bullet.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+@export var damage = 40
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_body_entered(body:Node):
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		queue_free()
